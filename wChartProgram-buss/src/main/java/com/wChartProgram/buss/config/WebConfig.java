f0989c8f1b66f76693eec9c5d43110bdf6e3b97d
王梦琦 <9603633+woshiwangmengqi@user.noreply.gitee.com>
2025-06-05 11:21:49 +0800

    取消拦截器注释

--- wChartProgram-buss/src/main/java/com/wChartProgram/buss/config/WebConfig.java
+++ wChartProgram-buss/src/main/java/com/wChartProgram/buss/config/WebConfig.java
@@ -1,21 +1,21 @@
-//package com.wChartProgram.buss.config;
-//
-//import com.wChartProgram.buss.interceptor.CustomerInterceptor;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Autowired
-//    private CustomerInterceptor customerInterceptor;
-//
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(customerInterceptor)
-//                .addPathPatterns("/**");//指定拦截的路径
-////                .excludePathPatterns("/program");//排除的路径
-//    }
-//}
+package com.wChartProgram.buss.config;
+
+import com.wChartProgram.buss.interceptor.CustomerInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private CustomerInterceptor customerInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(customerInterceptor)
+                .addPathPatterns("/**");//指定拦截的路径
+//                .excludePathPatterns("/program");//排除的路径
+    }
+}
--- wChartProgram-buss/src/main/java/com/wChartProgram/buss/interceptor/CustomerInterceptor.java
+++ wChartProgram-buss/src/main/java/com/wChartProgram/buss/interceptor/CustomerInterceptor.java
@@ -1,40 +1,40 @@
-//package com.wChartProgram.buss.interceptor;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.servlet.HandlerInterceptor;
-//import org.springframework.web.servlet.ModelAndView;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-///**
-// * 拦截器（拦截器优先级高于aop）
-// */
-//@Slf4j
-//@Component
-//public class CustomerInterceptor implements HandlerInterceptor {
-//
-//    @Override
-//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//        System.out.println("Pre-handle method");
-//        String url = request.getRequestURL().toString();
-//        log.info("请求的url: {}", url);
-//        if (url.contains("query")) {
-//            log.info("拦截器拦截-该路径不允许访问!");
-//            return false;
-//        }
-//        return HandlerInterceptor.super.preHandle(request, response, handler);
-//    }
-//
-//    @Override
-//    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-//        System.out.println("Post-handle method");
-//        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
-//    }
-//
-//    @Override
-//    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-//        System.out.println("sfter-handle method");
-//        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
-//    }
-//}
+package com.wChartProgram.buss.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 拦截器（拦截器优先级高于aop）
+ */
+@Slf4j
+@Component
+public class CustomerInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("Pre-handle method");
+        String url = request.getRequestURL().toString();
+        log.info("请求的url: {}", url);
+        if (url.contains("query")) {
+            log.info("拦截器拦截-该路径不允许访问!");
+            return false;
+        }
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("Post-handle method");
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("sfter-handle method");
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
